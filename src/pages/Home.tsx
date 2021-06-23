bdf87c1796972803b65686f186e922851794387e
Francisco Vicente A. Neto <vicenteneto.ti@gmail.com>
2021-06-23 17:21:22 -0300

    home page styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,34 +2,38 @@ import ilustration from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImage from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss'; 
+import { Button } from '../componentes/Button';
+
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside>
-                <img src={ilustration} alt="Ilustracao simbolizando perguntas e respostas" />
+                <img
+                    src={ilustration}
+                    alt="Ilustracao simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao vivo</strong>
                 <p>Tire as dúvidas de sua audiência em tempo real</p>
             </aside>
 
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                </div>
-                <button>
+                
+                <button className="create-room">
                     <img src={googleIconImage} alt="Logo do Google" />
                     crie sua sala com o google
                 </button>
-                <div>Ou entre em uma sala</div>
+                <div className="separator">Ou entre em uma sala</div>
                 <form>
                     <input
                         type="Text"
                         placeholder="Digite o codigo da sala"
                     />
-                    <button type="submit">
-                        Entrar na sala
-                    </button>
+                    <Button type="submit">Entrar na sala </Button>
                 </form>
+                </div>
             </main>
         </div>
-    )
+    );
 }
