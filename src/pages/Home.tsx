d40717980ddfc5da3c1a5e336f461e7367f0ca8e
Francisco Vicente A. Neto <vicenteneto.ti@gmail.com>
2021-06-24 14:25:29 -0300

    Authentication with firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,32 @@
 import { useHistory } from 'react-router-dom';
 
+import { auth, firebase } from "../services/firebase";
+
 import ilustration from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImage from '../assets/images/google-icon.svg';
 
-import '../styles/auth.scss'; 
+ 
 import { Button } from '../componentes/Button';
+
+import '../styles/auth.scss';
 
 export function Home() {
 
     const history = useHistory();
 
-    function navigateToNewRoom() {
-        history.push('/rooms/new');
+    function handleCreateRoom() {
+
+       const provider = new firebase.auth.GoogleAuthProvider();
+
+       auth.signInWithPopup(provider).then(result => {
+           
+          console.log(result);
+
+          history.push('/rooms/new');
+
+        })
+     
     }
 
     return (
@@ -29,7 +43,7 @@ export function Home() {
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                 
-                <button onClick={navigateToNewRoom} className="create-room">
+                <button onClick={handleCreateRoom} className="create-room">
                     <img src={googleIconImage} alt="Logo do Google" />
                     crie sua sala com o google
                 </button>
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,19 +1,22 @@
 import firebase from 'firebase/app';
 
-import "firebase/auth";
-import "firebase/firestore";
+import 'firebase/auth';
+import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: process.env.REAC_APP_API_KEY,
-    authDomain: process.env.REAC_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REAC_APP_DATABASE_URL,
-    projectId: process.env.REAC_APP_PROJECT_ID,
-    storageBucket: process.env.REAC_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REAC_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REAC_APP_APP_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+    
 };
 
 firebase.initializeApp(firebaseConfig)
 
-export const auth = firebase.auth();
-export const database = firebase.database();    
+const auth = firebase.auth();
+const database = firebase.database(); 
+
+export { firebase, auth, database }
