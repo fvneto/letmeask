bca0ae2d15bd332358fc6bdd87f6e5885262948b
Francisco Vicente A. Neto <vicenteneto.ti@gmail.com>
2021-06-24 06:30:08 -0300

    Room creation screen

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,37 @@
+import ilustration from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/auth.scss'; 
+import { Button } from '../componentes/Button';
+
+export function NewRoom() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img
+                    src={ilustration}
+                    alt="Ilustracao simbolizando perguntas e respostas" 
+                />
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire as dúvidas de sua audiência em tempo real</p>
+            </aside>
+
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                <form>
+                    <input
+                        type="Text"
+                        placeholder="Nome da Sala"
+                    />
+                    <Button type="submit">Criar sala </Button>
+                </form>
+                <p>
+                    Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+                </p>
+                </div>
+            </main>
+        </div>
+    );
+}
